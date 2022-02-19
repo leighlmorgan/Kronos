a6bdf588e2074bcbd64c486bf058e65f0849813c
Leigh Morgan <leigh@leighmorgan.com>
2022-02-19 14:55:56 +0000

    Just DB

--- KronosUI/KronosUI/My Project/Application.Designer.vb
+++ KronosUI/KronosUI/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.KronosUI.Form1
-        End Sub
+         Me.MainForm = Global.KronosUI.Admin
+      End Sub
     End Class
 End Namespace
